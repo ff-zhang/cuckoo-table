1fc5fd078208fedf4ad96169d655c23f8902adde
Felix Zhang <felixfzhang@hotmail.com>
2025-09-02 22:33:06 -0400

    move thread handling out of benchmark

--- tests/main.cpp
+++ tests/main.cpp
@@ -42,13 +42,14 @@ void run_test(const HugeVecT& read_idxs) {
   }
   slices[NUM_WORKERS] = NUM_REQUESTS;
 
-  // do lookups and measure throughput
-  std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-
   std::array<typename CuckooTableT::iterator, MAX_LOOKUP_BATCH_SZ> results{};
+  std::atomic<bool> flag = false;
   auto worker = [&](const size_t start, const size_t end) {
+    flag.wait(false);
     for (size_t i = start; i < end; i += MAX_LOOKUP_BATCH_SZ) {
-      table.find_batched(&read_idxs[i], MAX_LOOKUP_BATCH_SZ, results.data());
+      table.find_batched(
+        reinterpret_cast<const cuckoo_set::KeyT*>(&read_idxs[i]), MAX_LOOKUP_BATCH_SZ, results.data()
+      );
     }
   };
 
@@ -57,6 +58,11 @@ void run_test(const HugeVecT& read_idxs) {
     workers[i] = std::thread(worker, slices[i], slices[i + 1]);
   }
 
+  // do lookups and measure throughput
+  std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+
+  flag.store(true);
+  flag.notify_all();
   for (auto& t : workers) {
     t.join();
   }
